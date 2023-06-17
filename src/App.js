9ce4211cfe683465a724e072aeef358eb85e84f5
Ngufuli <ngufuli@outlook.com>
2023-06-18 05:14:49 +0530

    state, json props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [task, setTask] = useState([
+    {
+        "id": 1,
+        "body": "First Item",
+        "severity": 1,
+        "status": 0
+    },
+    {
+        "id": 2,
+        "body": "Second Item",
+        "severity": 2,
+        "status": 1
+    }
+])
+
   return (
     <div className="container">
-      <Header />      
+      <Header />     
+      <Tasks task={task}/> 
     </div>
   );
 }
