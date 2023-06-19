eef3501bce57e7ff832d9ea257b1b657829228bf
Ngufuli <ngufuli@outlook.com>
2023-06-19 11:23:22 +0530

    metho ui toggle

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,8 @@
 import {FaTimes} from 'react-icons/fa';
 
-const Task = ({tas, onDelete}) => {
+const Task = ({tas, onDelete, onToggle}) => {
     return(
-        <div className="task">
+        <div className={`task ${tas.reminder? 'reminder': ''} `} onDoubleClick={() =>onToggle(tas.id)}>
             <h3>{tas.body}<FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(tas.id)}/></h3>
             <p>{tas.severity}</p>
             <p>{tas.status}</p>
