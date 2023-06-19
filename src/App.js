2719a6fba74f241677be5e9d4c7168f773cfe2fe
Ngufuli <ngufuli@outlook.com>
2023-06-19 11:22:48 +0530

    toggle

--- src/App.js
+++ src/App.js
@@ -8,24 +8,39 @@ function App() {
         "id": 1,
         "body": "First Item",
         "severity": 1,
-        "status": 0
+        "status": 0,
+        "reminder": true
     },
     {
         "id": 2,
         "body": "Second Item",
         "severity": 2,
-        "status": 1
-    }
+        "status": 1,
+        "reminder": false
+    },
+    {
+      "id": 3,
+      "body": "Third Item",
+      "severity": 5,
+      "status": 6,
+      "reminder": true
+  }
 ])
 
+//Delete task
 const deleteTask = (id) => {
   setTask(task.filter((task)=> task.id !== id))
+}
+
+//Toggle reminder
+const toggleReminder = (id) => {
+  setTask(task.map((tas) => tas.id === id ? {...tas, reminder: !tas.reminder}: tas))
 }
 
   return (
     <div className="container">
       <Header />     
-      {task.length > 0 ? <Tasks task={task} onDelete={deleteTask}/> : 'No tasks available'}
+      {task.length > 0 ? <Tasks task={task} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks available'}
     </div>
   );
 }
